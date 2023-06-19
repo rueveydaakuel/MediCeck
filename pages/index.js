1d7b544b65abe93093706308b1770d0d188cc724
Rüveyda Akgül <rakguel94@gmail.com>
2023-06-19 11:48:34 +0200

    create homePage with sample data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,72 @@
-import Heading from "../components/Heading";
-import SubHeading from "../components/SubHeading";
+import React from "react";
+import styled from "styled-components";
+import data from "../data.json";
 
-export default function Home() {
+const Header = styled.h1`
+  border: 1px solid;
+  border-radius: 8px;
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 16px;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+const Card = styled.div`
+  border: 1px solid grey;
+  border-radius: 8px;
+  padding: 16px;
+  margin-bottom: 16px;
+  max-width: 400px;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PersonName = styled.h2`
+  font-size: 20px;
+  margin-bottom: 8px;
+`;
+
+const MedicationDetails = styled.div`
+  font-size: 16px;
+`;
+
+const MedicationTime = styled.p`
+  margin-bottom: 8px;
+`;
+
+const HomePage = () => {
   return (
-    <main>
-      <Heading>🐬 Capstone Template 🐬</Heading>
-      <SubHeading>We dolphinitely love coding!</SubHeading>
-    </main>
+    <div>
+      <HeaderContainer>
+        <Header>MediCheck</Header>
+      </HeaderContainer>
+      <CardContainer>
+        {data.people.map((person, index) => (
+          <Card key={index}>
+            <PersonName>{person.name}</PersonName>
+            <MedicationDetails>
+              {person.medication.map((medication, index) => (
+                <MedicationTime key={index}>
+                  {medication.time}: {medication.details}
+                </MedicationTime>
+              ))}
+            </MedicationDetails>
+          </Card>
+        ))}
+      </CardContainer>
+    </div>
   );
-}
+};
+
+export default HomePage;

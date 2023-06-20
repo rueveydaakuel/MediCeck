a2983e9761176f9eb3abed18800709b26c6f2f80
Rüveyda Akgül <rakguel94@gmail.com>
2023-06-20 14:21:24 +0200

    added some styling

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,15 +1,37 @@
-Navigation;
-
+import React from "react";
 import Link from "next/link";
+import styled from "styled-components";
+import Image from "next/image";
+
+import icon from "../../public/home-alt-svgrepo-com.svg";
 
 export default function Navigation() {
   return (
-    <nav>
+    <Nav>
       <ul>
-        <li>
-          <Link href="/">Homepage</Link>
-        </li>
+        <Li>
+          <a href="/">
+            <Image src={icon} alt="Icon" width={40} height={40} />
+          </a>
+        </Li>
       </ul>
-    </nav>
+    </Nav>
   );
 }
+
+const Nav = styled.nav`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  background-color: #d3d3d3;
+`;
+
+const Li = styled.li`
+  margin-right: 10px;
+  list-style-type: none;
+`;

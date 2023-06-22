0017b53ef5b61270b03c6a00472f1dad85449b1b
Rüveyda Akgül <rakguel94@gmail.com>
2023-06-22 11:26:01 +0200

    added some styling

--- pages/MedicationForm/index.js
+++ pages/MedicationForm/index.js
@@ -24,31 +24,33 @@ function FormComponent() {
   return (
     <div>
       <h2>Wann wird das Medikament eingenommen?</h2>
-
-      {["Mo", "Die", "Mi", "Do", "Fr", "Sa", "So"].map((medication, index) => (
-        <Box
-          key={index}
-          selected={selectedMedication === index}
-          onClick={() => handleMedicationSelection(index)}
-        >
-          {medication}
-        </Box>
-      ))}
-
+      <Container>
+        {["Mo", "Die", "Mi", "Do", "Fr", "Sa", "So"].map(
+          (medication, index) => (
+            <Box
+              key={index}
+              selected={selectedMedication === index}
+              onClick={() => handleMedicationSelection(index)}
+            >
+              {medication}
+            </Box>
+          )
+        )}
+      </Container>
       <h2>Zu welcher Tageszeit?</h2>
-
-      {["morgens", "mittags", "abends"].map((time, index) => (
-        <Box
-          key={index}
-          selected={selectedTime === index}
-          onClick={() => handleTimeSelection(index)}
-        >
-          {time}
-        </Box>
-      ))}
-
+      <Container>
+        {["morgens", "mittags", "abends"].map((time, index) => (
+          <Box
+            key={index}
+            selected={selectedTime === index}
+            onClick={() => handleTimeSelection(index)}
+          >
+            {time}
+          </Box>
+        ))}
+      </Container>
       <h2>Wie heißt das Medikament?</h2>
-      <input
+      <Input
         type="text"
         value={medicationName}
         onChange={handleMedicationNameChange}
@@ -62,6 +64,16 @@ export default FormComponent;
 const Box = styled.div`
   background-color: ${(props) => (props.selected ? "lightskyblue" : "silver")};
   color: ${(props) => (props.selected ? "white" : "black")};
+  padding: 10px;
+  margin: 5px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Input = styled.input`
   padding: 10px;
   margin: 5px;
 `;
